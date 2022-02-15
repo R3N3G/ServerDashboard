3c02c98ef862eb0d72edecf602f023ca4ffeda94
Florian Hoss <mail@florianhoss.de>
2022-02-15 18:46:37 +0100

    create custom css and filewatcher to style

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
 import System from "./components/System";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {APP_SITE_URL} from "./config";
 
 function App() {
     return (
-        <div className={"container position-absolute top-50 start-50 translate-middle card"}>
+        <div className={"container position-absolute top-50 start-50 translate-middle card bg-dark p-1"}>
             <div className={"card-body"}>
                 <System serverUrl={APP_SITE_URL}/>
             </div>
--- client/src/components/System.tsx
+++ client/src/components/System.tsx
@@ -6,7 +6,6 @@ import axios from "axios";
 
 const System: FC<Props> = ({serverUrl}) => {
     let webSocket;
-    let lala = process.env.APP_SITE_URL;
     const webSocketUrl = serverUrl.replace("http", "ws") + '/system/ws/';
     const staticSystemUrl = serverUrl + '/system/static/';
 
@@ -53,33 +52,27 @@ const System: FC<Props> = ({serverUrl}) => {
     return (
         <div>
             <h1 className={"mb-5 fw-bolder"}>Live-System</h1>
-            <div>VAR: {serverUrl}</div>
-            <div>ENV: {lala}</div>
-            <div className={"mb-5 card"}>
-                <div className={"card-body"}>
-                    <LiveSystem name={"CPU"} variant={"danger"}
-                                value={staticSystem.values.cpu}
-                                percentage={liveSystem.percentage.cpu}/>
-                    <LiveSystem name={"Disk"} variant={"primary"}
-                                value={liveSystem.values.disk + "/" + staticSystem.values.disk}
-                                percentage={liveSystem.percentage.disk}/>
-                    <LiveSystem name={"Memory"} variant={"warning"}
-                                value={liveSystem.values.ram + "/" + staticSystem.values.ram}
-                                percentage={liveSystem.percentage.ram}/>
-                </div>
+            <div className={"mb-5"}>
+                <LiveSystem name={"CPU"} variant={"danger"}
+                            value={staticSystem.values.cpu}
+                            percentage={liveSystem.percentage.cpu}/>
+                <LiveSystem name={"Memory"} variant={"warning"}
+                            value={liveSystem.values.ram + "/" + staticSystem.values.ram}
+                            percentage={liveSystem.percentage.ram}/>
+                <LiveSystem name={"Disk"} variant={"info"}
+                            value={liveSystem.values.disk + "/" + staticSystem.values.disk}
+                            percentage={liveSystem.percentage.disk}/>
             </div>
-            <div className={"card"}>
-                <div className={"card-body"}>
-                    <StaticSystem name={"OS"}
-                                  variant={"info"}
-                                  value={staticSystem.extras.operating_system}/>
-                    <StaticSystem name={"Architecture"}
-                                  variant={"info"}
-                                  value={staticSystem.extras.processor_architecture}/>
-                    <StaticSystem name={"Go"}
-                                  variant={"info"}
-                                  value={staticSystem.extras.go_version}/>
-                </div>
+            <div>
+                <StaticSystem name={"OS"}
+                              variant={"primary"}
+                              value={staticSystem.extras.operating_system}/>
+                <StaticSystem name={"Architecture"}
+                              variant={"primary"}
+                              value={staticSystem.extras.processor_architecture}/>
+                <StaticSystem name={"Go"}
+                              variant={"primary"}
+                              value={staticSystem.extras.go_version}/>
             </div>
         </div>
     );
